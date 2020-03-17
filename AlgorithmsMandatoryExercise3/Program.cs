cfd8affcdfeb2b17e2895da96e0a8bea170d5251
JeppeLarsen <Pnb97hhv!>
2020-03-17 15:13:44 +0100

    Initial commit

---
+++ AlgorithmsMandatoryExercise3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmsMandatoryExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            string indArray = Console.ReadLine();
+            int[] input = indArray.Split(' ').Select(str => int.Parse(str)).ToArray();
+        }
+    }
+}
