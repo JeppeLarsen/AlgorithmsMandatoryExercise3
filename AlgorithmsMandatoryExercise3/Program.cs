fa8a7011086c2ef02198544fb1758767b04aa719
JeppeLarsen <Pnb97hhv!>
2020-03-17 16:05:38 +0100

    ChristmasLightChain finished. CombustingConcoctions initiated.

--- AlgorithmsMandatoryExercise3/Program.cs
+++ AlgorithmsMandatoryExercise3/Program.cs
@@ -7,6 +7,40 @@ namespace AlgorithmsMandatoryExercise3
     {
         static void Main(string[] args)
         {
+            //ChristmasLightChain();
+            CombustingConcoctions();
+        }
+
+        static void ChristmasLightChain()
+        {
+            int length = int.Parse(Console.ReadLine());
+            string indArray = Console.ReadLine();
+            int[] input = indArray.Split(' ').Select(str => int.Parse(str)).ToArray();
+            Array.Sort(input);
+            //Console.WriteLine(string.Join(' ', input));
+
+            int maxCons = 0;
+            int currentCount = 1;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] - 1 == input[i - 1])
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    currentCount = 1;
+                }
+
+                if (currentCount > maxCons) maxCons = currentCount;
+            }
+
+            Console.WriteLine(maxCons == 1 ? 0 : maxCons);
+        }
+
+        static void CombustingConcoctions()
+        {
             int length = int.Parse(Console.ReadLine());
             string indArray = Console.ReadLine();
             int[] input = indArray.Split(' ').Select(str => int.Parse(str)).ToArray();
